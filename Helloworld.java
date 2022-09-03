f85b698eb87361ac3c20dc27613bccda71e97bc2
root <root@ip-172-31-6-201.eu-west-2.compute.internal>
2022-09-03 21:10:41 +0000

    Restored the file from second commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,15 +1,8 @@
-// This program will print simple message
 public class HelloWorld {
    public static void main(String[] args) {
  
 
      // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-    //  Prints "Welcome to Git" in the terminal window.
-
-     System.out.println("Welcome to GIT !!");
-
-
-
    }
 }
