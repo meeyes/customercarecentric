58ceac000b841e9e4e8d9318be3a6d7382fc3d43
root <root@ip-172-31-6-201.eu-west-2.compute.internal>
2022-09-03 20:10:11 +0000

    Added a new print statement

--- Helloworld.java
+++ Helloworld.java
@@ -5,5 +5,10 @@ public class HelloWorld {
 
      // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
+
+     System.out.println("Welcome to GIT !!");
+
+
+
    }
 }
